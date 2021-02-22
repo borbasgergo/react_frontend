1504b5084467df3a92859d195175fe39c41ffc4a
borbasgergo <59198924+borbasgergo@users.noreply.github.com>
2021-02-22 19:16:40 +0100

    Delete src directory

--- src/redux/store.ts
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import combinedReducers from "./reducer";
-
-export const store = createStore(combinedReducers, applyMiddleware(thunk))
--- src/types/ActionTypes.ts
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = "ADD_TODO"
-export const DELETE_TODO = "DELETE_TODO"
-export const SET_TODO = "SET_TODO"
-
-export const CHANGE_THEME = "CHANGE_THEME"
